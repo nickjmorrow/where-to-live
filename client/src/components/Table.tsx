e26cf24296c04d032145a994fb9f803f97acf094
Nicholas Morrow <njmorrow95@gmail.com>
2020-01-28 21:08:07 -0500

    Sort when data is refreshed.

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import React from 'react';
 import { useTable, useSortBy } from 'react-table';
 import { useThemeContext, Theme } from '@nickjmorrow/react-component-library';
-import { getCities, getMetricGroups, getVisibleCitiesSelector } from 'reduxUtilities/uiSelectors';
+import { getMetricGroups, getVisibleCitiesSelector } from 'reduxUtilities/uiSelectors';
 import { useSelector } from 'react-redux';
 import { MetricGroup } from 'types/MetricGroup';
 import Flip from 'react-flip-move';
@@ -50,16 +50,18 @@ const TableInternal: React.FC = () => {
 			</Heading>
 
 			<Flip style={{ display: 'table-row-group', height: '600px' }} {...getTableBodyProps()}>
-				{rows.map((row, i) => {
-					prepareRow(row);
-					return (
-						<Row {...row.getRowProps()}>
-							{row.cells.map(cell => {
-								return <Cell {...cell.getCellProps()}>{cell.render('Cell')}</Cell>;
-							})}
-						</Row>
-					);
-				})}
+				{rows
+					.sort((a, b) => (a.original.score > b.original.score ? -1 : 1))
+					.map((row, i) => {
+						prepareRow(row);
+						return (
+							<Row {...row.getRowProps()}>
+								{row.cells.map(cell => {
+									return <Cell {...cell.getCellProps()}>{cell.render('Cell')}</Cell>;
+								})}
+							</Row>
+						);
+					})}
 			</Flip>
 		</StyledTable>
 	);
